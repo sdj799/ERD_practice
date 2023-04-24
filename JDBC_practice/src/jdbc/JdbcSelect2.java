96ebeac9428a7fe9669ca30bdd3b2bd776f829ec
dongjun <sdj799@naver.com>
2023-04-24 15:36:55 +0900

    04.24. commit

---
+++ JDBC_practice/src/jdbc/JdbcSelect2.java
@@ -0,0 +1,71 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class JdbcSelect2 {
+
+	public static void main(String[] args) {
+
+		/*
+		 사용자에게 Scanner를 이용하여 job_id를 입력받습니다.
+		 입력받은 job_id에 해당하는 사람들의 first_name, salary를
+		 콜솔창에 출력해 주세요. (employees 테이블 사용.)
+		 조회된 내용이 없다면 조회 결과가 없다고 출력해 주세요.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("job_id: ");
+		String id = sc.next();
+		
+		String sql = "SELECT * FROM employees WHERE job_id = ?";
+		
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String uid = "hr";
+		String upw = "hr";
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, uid, upw);
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			int count = 0;
+			while(rs.next()) {
+				String firstName = rs.getString("first_name");
+				int salary = rs.getInt("salary");
+				System.out.printf("이름: %s salary: %d\n", firstName, salary);
+				count++;
+				System.out.println("--------------------");
+				
+			}
+			if(count == 0) {
+				System.out.println("해당 job_id에 대한 조회 결과가 없습니다");
+			} else {
+				System.out.println("조회된 행의 개수 " + count + "개");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+				sc.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
